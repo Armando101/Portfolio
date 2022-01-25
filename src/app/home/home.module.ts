dfa93509469546abb55e7861f07b26d45c2f3791
Armando Rivera <rivera.armando997@gmail.com>
2022-01-24 21:24:31 -0600

    Experience list component created

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,9 +7,11 @@ import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './home/home.component';
 import { ProjectComponent } from './project/project.component';
 import { ProjectListComponent } from './project-list/project-list.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ExperienceListComponent } from './experience-list/experience-list.component';
 
 @NgModule({
-  declarations: [HeroComponent, HomeComponent, ProjectComponent, ProjectListComponent],
+  declarations: [HeroComponent, HomeComponent, ProjectComponent, ProjectListComponent, ExperienceComponent, ExperienceListComponent],
   imports: [CommonModule, HomeRoutingModule, SharedModule],
 })
 export class HomeModule {}
