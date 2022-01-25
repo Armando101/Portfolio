34c8b560dcac7a526b2cde0cbc2c7b5662c5f4a3
Armando Rivera <rivera.armando997@gmail.com>
2022-01-24 19:44:57 -0600

    Hero and button component generated

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
-  declarations: [HeaderComponent, NavbarComponent],
+  declarations: [HeaderComponent, NavbarComponent, ButtonComponent],
   imports: [CommonModule],
-  exports: [HeaderComponent, NavbarComponent],
+  exports: [HeaderComponent, NavbarComponent, ButtonComponent],
 })
 export class SharedModule {}
