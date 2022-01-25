033129c88d50d9ed9120b1fd80969caed6bccc3c
Armando Rivera <rivera.armando997@gmail.com>
2022-01-24 18:46:48 -0600

    Header and navbar components functional

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   name = 'Armando Rivera';
+  showNav = false;
+  @HostListener('document:click', ['$event'])
+  clickout(event) {
+    if (this.eRef.nativeElement.contains(event.target)) {
+      return;
+    }
+    this.showNav = false;
+  }
+
+  constructor(private eRef: ElementRef) {}
+
+  showNavbar() {
+    this.showNav = !this.showNav;
+  }
+
+  get hamburguerIcon() {
+    return 'icon-hamburguer-button';
+  }
+
+  get closeIcon() {
+    return 'icon-close';
+  }
+
+  get currentIconClass() {
+    const iconClass = this.showNav ? this.closeIcon : this.hamburguerIcon;
+    return iconClass;
+  }
 }
