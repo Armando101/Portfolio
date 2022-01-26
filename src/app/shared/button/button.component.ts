ed806767c14ffdfad64e147c462e64a2579bd982
Armando Rivera <rivera.armando997@gmail.com>
2022-01-26 14:00:16 -0600

    Hero responsive

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-
+const DEFAULT_BUTTON_SIZE = 100;
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
@@ -8,4 +8,15 @@ import { Component, Input } from '@angular/core';
 export class ButtonComponent {
   @Input() title: string;
   @Input() type: string;
+  @Input() private readonly size: number;
+  @Input() private readonly breakPoint: string;
+
+  public get inlineSize(): string {
+    const buttonSize = this.size || DEFAULT_BUTTON_SIZE;
+    if (this.breakPoint) {
+      return `w-${this.breakPoint}-${buttonSize}`;
+    }
+
+    return `w-${buttonSize}`;
+  }
 }
